391a0dfbf0a5dac89fb5670b9db958255b909813
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-09-28 16:39:18 -0500

    Se termina el programa de los signos zodiacales

--- main.py
+++ main.py
@@ -1,1 +1,97 @@
-print("Hola Mundo")
+day = int(input("Ingrese el día porfavor: "))
+month = int(input("Ingrese el mes: "))
+#Capricornio
+if month == 12:
+    if day >= 22 and day<=31:
+        print("Capricornio")
+elif month == 1:
+    if day >= 1 and day<=20:
+        print("Capricornio")
+
+#Acuario
+if month == 1:
+    if day >= 21 and day<=31:
+        print("Acuario")
+elif month == 2:
+    if day >= 1 and day<=19:
+        print("Acuario")
+
+#piscis
+if month == 2:
+    if day >= 20 and day<=28:
+        print("Piscis")
+elif month == 3:
+    if day >= 1 and day<=20:
+        print("Piscis")
+
+#Aries
+if month == 3:
+    if day >= 21 and day<=31:
+        print("aries")
+elif month == 4:
+    if day >= 1 and day<=20:
+        print("aries")
+
+#Tauro
+if month == 4:
+    if day >= 21 and day<=30:
+        print("Tauro")
+elif month == 5:
+    if day >= 1 and day<=21:
+        print("Tauro")
+
+#Geminis
+if month == 5:
+    if day >= 22 and day<=31:
+        print("Géminis")
+elif month == 6:
+    if day >= 1 and day<=21:
+        print("Géminis")
+
+#Cáncer
+if month == 6:
+    if day >= 22 and day<=30:
+        print("Cáncer")
+elif month == 7:
+    if day >= 1 and day<=23:
+        print("Cáncer")
+
+#Leo
+if month == 7:
+    if day >= 24 and day<=31:
+        print("Leo")
+elif month == 8:
+    if day >= 1 and day<=23:
+        print("Leo")
+
+#Virgo
+if month == 8:
+    if day >= 24 and day<=31:
+        print("Virgo")
+elif month == 9:
+    if day >= 1 and day<=23:
+        print("Virgo")
+
+#Libra
+if month == 9:
+    if day >= 24 and day<=30:
+        print("Libra")
+elif month == 10:
+    if day >= 1 and day<=23:
+        print("Libra")
+
+#Escorpio
+if month == 10:
+    if day >= 24 and day<=31:
+        print("Escorpio")
+elif month == 11:
+    if day >= 1 and day<=22:
+        print("Escorpio")
+
+#Sagitario
+if month == 11:
+    if day >= 23 and day<=30:
+        print("Sagitario")
+elif month == 12:
+    if day >= 1 and day<=21:
+        print("Sagitario")
