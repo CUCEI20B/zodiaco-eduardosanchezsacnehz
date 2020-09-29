61998409871d59ce4d98005882c58d141a375e0e
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-09-28 21:18:43 -0500

    Pruevafinal

--- main.py
+++ main.py
@@ -1,86 +1,86 @@
-day = int(input("Ingrese el dia "))
-month = int(input("Ingrese el mes "))
+dia = int(input("Ingresa dia "))
+mes = int(input("Ingresa mes "))
 
-if month == 12:
-    if day >= 22 and day<=31:
-        print("Capricornio")
-elif month == 1:
-    if day >= 1 and day<=20:
-        print("Capricornio")
+if mes == 3:
+    if dia >= 23 and dia <= 31:
+        print ("aries")
+elif mes == 4:
+    if dia >= 1 and dia <= 20:
+        print ("aries")
 
-if month == 1:
-    if day >= 21 and day<=31:
-        print("Acuario")
-elif month == 2:
-    if day >= 1 and day<=19:
-        print("Acuario")
+if mes == 4:
+   if dia >= 21 and dia <= 30:
+        print ("tauro")
+elif mes == 5:
+    if dia >= 1 and dia <= 20:
+        print ("tauro")
 
-if month == 2:
-    if day >= 20 and day<=28:
-        print("Piscis")
-elif month == 3:
-    if day >= 1 and day<=20:
-        print("Piscis")
+if mes == 5:
+   if dia >= 21 and dia <= 31:
+        print ("geminis")
+elif mes == 6:
+    if dia >= 1 and dia <= 21:
+        print ("geminis")
 
-if month == 3:
-    if day >= 21 and day<=31:
-        print("aries")
-elif month == 4:
-    if day >= 1 and day<=20:
-        print("aries")
+if mes == 6:
+   if dia >= 22 and dia <= 30:
+        print ("cancer")
+elif mes == 7:
+    if dia >= 1 and dia <= 22:
+        print ("cancer")
 
-if month == 4:
-    if day >= 21 and day<=30:
-        print("Tauro")
-elif month == 5:
-    if day >= 1 and day<=21:
-        print("Tauro")
+if mes == 7:
+   if dia >= 23 and dia <= 31:
+        print ("leo")
+elif mes == 8:
+    if dia >= 1 and dia <= 22:
+        print ("leo")
 
-if month == 5:
-    if day >= 22 and day<=31:
-        print("Geminis")
-elif month == 6:
-    if day >= 1 and day<=21:
-        print("Geminis")
+if mes == 8:
+   if dia >= 23 and dia <= 31:
+        print ("virgo")
+elif mes == 9:
+    if dia >= 1 and dia <= 22:
+        print ("virgo")
 
-if month == 6:
-    if day >= 22 and day<=30:
-        print("Cancer")
-elif month == 7:
-    if day >= 1 and day<=23:
-        print("Cancer")
+if mes == 9:
+   if dia >= 23 and dia <= 30:
+        print ("libra")
+elif mes == 10:
+    if dia >= 1 and dia <= 22:
+        print ("libra")
 
-if month == 7:
-    if day >= 24 and day<=31:
-        print("Leo")
-elif month == 8:
-    if day >= 1 and day<=23:
-        print("Leo")
+if mes == 10:
+   if dia >= 23 and dia <= 31:
+        print ("escorpio")
+elif mes == 11:
+    if dia >= 1 and dia <= 22:
+        print ("escorpio")
 
-if month == 8:
-    if day >= 24 and day<=31:
-        print("Virgo")
-elif month == 9:
-    if day >= 1 and day<=23:
-        print("Virgo")
+if mes == 11:
+   if dia >= 23 and dia <= 30:
+        print ("sagitario")
+elif mes == 12:
+    if dia >= 1 and dia <= 21:
+        print ("sagitario")
 
-if month == 9:
-    if day >= 24 and day<=30:
-        print("Libra")
-elif month == 10:
-    if day >= 1 and day<=23:
-        print("Libra")
+if mes == 12:
+   if dia >= 22 and dia <= 31:
+        print ("capricornio")
+elif mes == 1:
+    if dia >= 1 and dia <= 20:
+        print ("capricornio")
 
-if month == 10:
-    if day >= 24 and day<=31:
-        print("Escorpio")
-elif month == 11:
-    if day >= 1 and day<=22:
-        print("Escorpio")
+if mes == 1:
+   if dia >= 21 and dia <=31:
+        print ("acuario")
+elif mes == 2:
+    if dia >= 1 and dia <= 18:
+        print ("acuario")
 
-if month == 11:
-    if day >= 23 and day<=30:
-        print("Sagitario")
-elif month == 12:
-    if day >= 1 and day<=21:
-        print("Sagitario")
+if mes == 2:
+   if dia >= 19 and dia <=28:
+        print ("piscis")
+elif mes == 3:
+    if dia >= 1 and dia <= 20:
+        print ("piscis"
